2440ee367628d7e5c4846598ad31ec57f7d4164a
Mark Ballinger <markballinger@optusnet.coml.au>
2022-08-10 10:26:42 +1000

    Removed marble madness scripts
    was causing an issue with pause started again in marblemadnesv2

--- Assets/Scripts/Marblemadness/ProjectConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace SAE.Mark.Ballinger.GAM405.Shared
-{
-    public static class ProjectConstants
-    {
-        public const string FloorLayer = "Floor";
-        public const string RampLayer = "Ramp";
-    }
-}
