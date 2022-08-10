cdd0bb7483100584d99b659a84b905708e1f6270
Mark Ballinger <markballinger@optusnet.coml.au>
2022-08-10 11:56:59 +1000

    added game manager and play again button
    Having trouble with play again button and overlay not going away will sort on next push

---
+++ Assets/Scripts/MarbleMadnessV2/Points.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    public int value = 10;
+    public GameObject explosionPrefab;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            if (GameManager.gm!=null)
+            {
+                // tell the game manager to Collect
+                GameManager.gm.Collect (value);
+            }
+			
+            // explode if specified
+            if (explosionPrefab != null) {
+                Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+            }
+			
+            // destroy after collection
+            Destroy (gameObject);
+        }
+    }
+}
